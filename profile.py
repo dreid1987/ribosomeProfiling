04bec0867a3db747a25737fe4b577dd81dc3ef5f
dreid1987 <dreid1987@gmail.com>
2015-05-04 18:27:51 +0800

    Update metagene generation

--- profile.py
+++ profile.py
@@ -1,146 +1,173 @@
+readCutoff=3
+individReadCutoff=2500
+posxmin=-20
+posxmax=125
+ribo5Add=14
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 import os
-import sys
 
+class mdict(dict):
 
-def which(program): #From http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
-	def is_exe(fpath):
-		return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    def __setitem__(self, key, value):
+        """add the given value to the list of values for this key"""
+        self.setdefault(key, []).append(value)
 
-	fpath, fname = os.path.split(program)
-	if fpath:
-		if is_exe(program):
-			return program
-	else:
- 		for path in os.environ["PATH"].split(os.pathsep):
-			path = path.strip('"')
-			exe_file = os.path.join(path, program)
-			if is_exe(exe_file):
-				return exe_file
-
-	return ""
-def addNextFolder(currentFolder):
-	folders=os.listdir(currentFolder)
-	return currentFolder + folders[0] +'/'
-
-
-
-print 'Checking dependencies...'
-print 'You may be asked to enter your OS password if dependencies need to be installed'
-
-deps=dict()
-deps['pip']='python-pip'
-deps['samtools']='samtools'
-deps['bowtie']='bowtie'
-deps['cufflinks']='cufflinks'
-deps['tophat']='tophat'
-
-for dep in deps.keys():
-	if len(which(dep))==0:
-		os.system('sudo apt-get install ' + deps[dep])
-
-if len(which('cutadapt'))==0:
-	os.system('sudo pip install cutadapt')
-try:
-	import matplotlib.pyplot as plt
-except ImportError:
-	os.system('sudo pip install matplotlib')
-try:
-	import Bio
-except ImportError:
-	os.system('sudo apt-get install python-biopython')
-
-
-print '\n\nWhat is the name of your reference genome file?'
-print 'Before running this program, download a Refseq genome and extract it into a folder called \'database\' that is inside your currect directory'
-print 'You may download these from http://ccb.jhu.edu/software/tophat/igenomes.shtml.'
-print 'Example input: Homo_sapiens'
-refFolder = raw_input('> ')
-
-
-
-#Find database
-databaseFolder='database/' + refFolder + '/'
-i=0
-while 'Annotation' not in os.listdir(databaseFolder) and i<6:
-	databaseFolder=addNextFolder(databaseFolder)
-	i+=1
-	
-gtfOK = os.path.isfile(databaseFolder + 'Annotation/Genes/genes.gtf') 
-if gtfOK:
-	gtfLoc=databaseFolder + 'Annotation/Genes/genes.gtf'
-else:	
-	while not gtfOK:
-		gtfLoc=raw_input('GTF file not found. Specify GTF file: ')
-		gtfOK = os.path.isfile(gtfLoc) 
-chrOK = os.path.isfile(databaseFolder + "Sequence/Chromosomes/1.fa") 
-if chrOK:
-	chrLoc=databaseFolder + "Sequence/Chromosomes/"
-else:	
-	while not chrOK:
-		chrLoc=raw_input('Genome sequence files not found. Specify folder with .fa files: ')	
-		chrOK = os.path.isfile(chrLoc + '1.fa') 
-
-if not os.path.isdir('trim'):
-	os.mkdir('trim')
-if not os.path.isdir('mapped'):
-	os.mkdir('mapped')
-
-
-
-readFiles=os.listdir('reads/')
-touse=[]
-for fName in readFiles:
-	if fName.split('.')[1]=='fastq':
-		touse.append(fName)
-readFiles=touse
-print '\n\nWhat is the 3\' adapter to trim?'
-print "Enter 'N' to use the default NEB or Illumina adapters, or enter your own sequence"
-toTrim = raw_input('> ')
-if toTrim=='N':
-	toTrim='AAGATCGGAAGAGCACACGTCT'
-	
-print '\n Trimming adapters from reads using Cutadapt'	
-for readFile in readFiles:
-	os.system('cutadapt -a ' + toTrim +' --discard-untrimmed -m 20 reads/' + readFile + ' -o trim/' + readFile)
-
-print '\n Building a transcriptome using Tophat Cufflinks'	
-tophatCommand= 'tophat -G ' + gtfLoc + ' -T -p 3 --bowtie1 ' + databaseFolder + 'Sequence/BowtieIndex/genome '
-readFilePos=1
-for readFile in readFiles:
-	tophatCommand = tophatCommand + 'trim/' + readFile
-	if readFilePos<len(readFiles):
-		tophatCommand = tophatCommand +','
-os.system(tophatCommand)
+def makeHistoList(list,xstart=0,xstop=None,normalizeToOne=True):
+	if xstop==None:
+		xtop=max(list)
+	list.sort();listLen=len(list)
+	out=[]
+	pos=0
+	for i in range(xstart,xstop):
+		count=list.count(i)
 		
-os.system('cufflinks -m 35 -s 5 -G ' + databaseFolder + 'Annotation/Genes/genes.gtf tophat_out/accepted_hits.bam')
+		out.append(count)
+	if normalizeToOne:
+		s=np.sum(out)
+		newY=[]
+		for i in out:
+			newY.append(float(i)/s*100)
+		out=newY
+	return out
+	
+def writePosGraphs(listOfPos,xmin,xmax,name,gene="",normalize=1):
+
+	y=makeHistoList(listOfPos,xstart=xmin,xstop=xmax,normalizeToOne=True)
+	
+	
+	x=range(xmin,xmax)
+	
+	
+	if len(gene)>0:
+		fill=[1]*(cdsStop[gene]-cdsStart[gene])
+		plt.fill_between(range(cdsStart[gene],cdsStop[gene]),0,max(y),color=(0,0,0))
+		
+	write=open(name + '.txt','w')
+	write.writelines('Position on mRNA\tPercentage of reads')
+	
+	for i in range(len(y)):
+		write.writelines('\n' + str(x[i]) + "\t" + str(y[i]))
+		
+	write.close()
+	plt.plot(x,y,color=(1,0,0))
+	
+	plt.xlabel('Position in mRNA')
+	plt.ylabel('Percentage of reads')
+	plt.axis(xmin=xmin,xmax=xmax,ymax=max(y))
+	plt.savefig(name + '.svg')
+	plt.savefig(name + '.png')
+	plt.clf()
+	
+	return y
+
+def loadReads(folder,fileName):
+	file = folder + '/' +fileName 
+	print '\t   ' + fileName.strip('.map')
+	thisCount=dict()
+	c=0
+	geneLevel=dict()
+	individGenes=mdict()
+	try:
+		for line in open(file):
+			line=line.split("\t")
+			
+			if len(line)>1:
+				pos=int(line[3]) + ribo5Add
+				gene=line[2]
+				try:
+					
+					if pos >cdsStart[gene] and pos < cdsStop[gene]:
+						try:
+							thisCount[line[2]]+=1
+						except KeyError:
+							thisCount[line[2]]=1
+						c+=1
+					individGenes[gene]=pos
+				except KeyError: pass
+		
+		
+		for gene in thisCount.keys():
+			if thisCount[gene]>readCutoff and cdsStop[gene]-cdsStart[gene]>10:
+				rpkm=10**9*thisCount[gene]/(float(c)*(cdsStop[gene]-cdsStart[gene]))
+				geneLevel[gene]=rpkm
+		
+		metageneProfiles=[]	
+		for gene in geneLevel.keys():			
+			if len(individGenes[gene])>=individReadCutoff:
+				thisGeneDist=writePosGraphs(individGenes[gene],0,mRNALen[gene],'plots/individGenes/' + refToCommon[gene] +'-'+ fileName.strip('.map'),gene=gene)
+				
+				#if geneLevel[gene]>rpkmCutoff:
+				if cdsStart[gene]> posxmin*-1 and cdsStop[gene]-cdsStart[gene]>posxmax:	
+					metaBuild=thisGeneDist[cdsStart[gene]-20:cdsStart[gene]+posxmax]
+					if np.sum(metaBuild)>0:
+						metageneProfiles.append(np.array(metaBuild)/np.sum(metaBuild))
+					
+		#Make metagene profile
+		dist=[]
+		write=open('plots/riboPosition/' + prefix+t,'w')
+		x=range(posxmin,posxmax)
+		for pos in range(posxmax-posxmin):
+			build=[]
+			for gene in metageneProfiles:
+				try:
+					build.append(gene[pos])
+				except KeyError:pass
+			dist.append(np.mean(build))
+			write.writelines('\n' + str(x[pos]) + '\t' + str(dist[pos]))
+		write.close()
+		plt.plot(range(posxmin,posxmax),dist,color=(1,0,0))
+		plt.xlabel('Position relative to start')
+		plt.ylabel('Percentage of reads')
+		plt.axis(xmin=posxmin,xmax=posxmax,ymin=0)
+		plt.savefig('plots/riboPosition/' + fileName.strip('.map') + '.png')
+		plt.savefig('plots/riboPosition/' + fileName.strip('.map') + '.svg')
+		plt.clf()
+		
+		
+	except IOError:
+		print 'Missing: ' + fileName
+	
+	return geneLevel
+	
+	
+
+cdsStart=dict();cdsStop=dict();refToCommon=dict();commToRef=dict();mRNALen=dict()
+fullName=dict()	
+memProt=dict()
+dengueResponse=dict()
+allGenes=[]
+for line in open('database/geneInfo'):
+	if len(line)>4 and line[0] !="#":
+		line=line.strip("\n").split("\t")
+		allGenes.append(line[0])
+		cdsStart[line[0]]=int(line[2]);cdsStop[line[0]]=int(line[3]);mRNALen[line[0]]=int(line[4]);commToRef[line[1]]=line[0];	refToCommon[line[0]]=line[1]
+
+#Load footprints
+print 'Printing graphs to the plots/ folder...'
+files=os.listdir('mapped/')
+for lib in files:
+	vars()[lib.strip('.map') + 'abund']=loadReads('mapped',lib)
+
+		
+print 'Writing gene expression levels (totalExpression.csv)'
+write=open('totalExpression.csv','w')
 
 
+write.writelines('\t')
+for sample in files:
+	write.writelines('\t' + sample.strip('.map'))
+for gene in allGenes:
+	write.writelines('\n' + gene + '\t' + refToCommon[gene])
+	for sample in files:
+		
+		try:
+			toWrite='\t' + str(vars()[sample.strip('.map') + 'abund'][gene])
+		except KeyError:
+			toWrite='\t'
+		write.writelines(toWrite)
+write.close()
 
-os.system('python chooseTranscripts.py ' + gtfLoc + ' ' + chrLoc)
-os.system('bowtie-build database/chosenTranscripts database/chosenTranscripts')
-for readFile in readFiles:
-	os.system('bowtie -p 3 --norc -v 0 database/chosenTranscripts trim/' + readFile + ' mapped/' + readFile.split('.')[0] + '.map')
-
-for fileName in readFiles:
-	if os.path.getsize('mapped/' + fileName.split('.')[0] + '.map') < 10000:
-		print '\n\nMapping of ' + fileName.split('.')[0] + ' failed. Were the correct adapters trimmed? Check your library preparation protocol and make sure that you entered the sequence that was appended to the 3\' end of your RNA.'
-		print 'Do you want to see a sampling of reads from your file? The adapter is the sequence that is shared among all of these sequences towards the end.'
-		seeReads= raw_input('(Y/N) > ')
-		if seeReads=='Y' or seeReads=='y' or seeReads=='yes':
-			with open('reads/' + fileName) as seqfile:
-				first = [next(seqfile) for x in xrange(40)]
-			i=0
-			for line in first:
-				if i%4==1:
-					print line.strip('\n')
-		sys.exit()
-
-
-if not os.path.isdir('plots'):
-	os.system('mkdir plots')
-if not os.path.isdir('plots/riboPosition'):
-	os.system('mkdir plots/riboPosition')
-if not os.path.isdir('plots/individGenes'):
-	os.system('mkdir plots/individGenes')
-
-os.system('python diff.py')
+print 'Done! ' + str(len(files)) + ' libraries analyzed.'
